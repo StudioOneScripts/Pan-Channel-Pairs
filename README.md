1f94926d0ca68bd0c3694e876dc7f6065682a70c
Your Name <expressmix@att.net>
2022-10-17 06:49:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pan Channel Pairs
+
+This function automatically hard pans selected mixer channels Left and Right in series
---
+++ code.js
@@ -0,0 +1,39 @@
+﻿function userFunction()
+{
+	this.interfaces =  [Host.Interfaces.IEditTask]
+
+	this.prepareEdit = function (context)
+	{
+		return Host.Results.kResultOk;
+	}
+
+	// -----------------------------------------------------------------
+
+	this.performEdit = function (context)
+	{
+		var environment = context.functions.root.environment;
+		var console = environment.find ("MixerConsole");
+		var channelList = console.getChannelList(1);
+
+		for(i=0;i < channelList.numSelectedChannels; i++)
+			{
+				var channel = channelList.getSelectedChannel(i)
+				
+				if (i % 2 == 0)
+					channel.pan = 1;
+				else
+					channel.pan = 0;
+			}
+
+		return Host.Results.kResultOk;
+	}
+
+}
+
+// ---------------------------------------------------------------------
+function createInstance()
+{
+	return new userFunction();
+}
+
+
