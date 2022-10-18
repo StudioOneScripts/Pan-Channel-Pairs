9205540402b5fec3c9af9fee4958bc4f5193d7f5
Your Name <expressmix@att.net>
2022-10-18 05:45:48 -0400

    Updated comments

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-﻿function userFunction()
+﻿function panChannelPairs()
 {
 	this.interfaces =  [Host.Interfaces.IEditTask]
 
@@ -11,29 +11,29 @@
 
 	this.performEdit = function (context)
 	{
+		// opposite pan pairs of selected channels
 		var environment = context.functions.root.environment;
 		var console = environment.find ("MixerConsole");
 		var channelList = console.getChannelList(1);
 
 		for(i=0;i < channelList.numSelectedChannels; i++)
-			{
-				var channel = channelList.getSelectedChannel(i)
-				
-				if (i % 2 == 0)
-					channel.pan = 1;
-				else
-					channel.pan = 0;
-			}
+		{ 
+			var channel = channelList.getSelectedChannel(i)
+	
+			if (i % 2 == 0)  		// odd 	 
+				channel.pan = 1;   	// left
+			else					// even 	
+				channel.pan = 0;  	// right
+		}
 
 		return Host.Results.kResultOk;
 	}
-
 }
 
 // ---------------------------------------------------------------------
 function createInstance()
 {
-	return new userFunction();
+	return new panChannelPairs();
 }
 
 
